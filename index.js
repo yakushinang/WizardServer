60f663abb8ff668c331d0c43d2169bbdac439a27
yakushinang <yakushin.ang@gmail.com>
2022-03-05 12:59:26 +0200

    wizard server

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express'); 
+const cors = require("cors");
+const ticketConfig = require('./data/ticketLowcost.json')
+
+const app = express();             
+const port = 5000;      
+
+const corsOptions ={
+    origin:'*', 
+    optionSuccessStatus:200,
+ }
+app.use(cors(corsOptions))
+
+
+app.get('/ticketWizard/:type', (req, res) => {   
+    console.log("ticketConfig", ticketConfig)   
+    console.log("ticketConfig", req.params.type)   
+    const type= req.params.type
+    const typeConfig = ticketConfig[type] || ''
+    if(typeConfig){
+       return res.json(typeConfig);  
+    }
+    return res.status(400).send({
+        message: 'This is an error!'
+     });
+    
+
+});
+
+app.listen(port, () => { 
+    console.log(`Now listening on port ${port}`); 
+});
